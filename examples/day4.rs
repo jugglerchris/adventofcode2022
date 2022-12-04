ccd9f1a56a356e45a00c0d26bfa95f498e4cc060
Chris Emerson <github@mail.nosreme.org>
2022-12-04 05:27:37 +0000

    Day 4 part 1.

--- examples/day4.rs
+++ examples/day4.rs
@@ -1,12 +1,29 @@
 #[allow(unused)]
-use adventofcode2022::{get_input,parse_lines};
+use adventofcode2022::{get_input,parse_lines,regex_parser};
+use std::ops::Range;
 
-type Data = ();
+#[derive(Clone,Debug)]
+pub struct Info(Range<usize>, Range<usize>);
+
+regex_parser!(parse_elf: Info {
+    RE = r#"^(\d+)-(\d+),(\d+)-(\d+)"# =>
+        |x0: usize, x1: usize, y0: usize, y1: usize|
+          Info(Range { start: x0, end: x1 },
+            Range { start: y0, end: y1 })
+});
+
+type Data = Vec<Info>;
 fn parse_input(input: &str) -> Data {
+    parse_lines(input)
 }
 
 fn part1(data: &Data) -> usize {
-    unimplemented!()
+    data.iter()
+        .filter(|Info(r1, r2)| {
+            (r1.start <= r2.start && r1.end >= r2.end) ||
+            (r2.start <= r1.start && r2.end >= r1.end)
+          })
+        .count()
 }
 fn part2(data: &Data) -> usize {
     unimplemented!()
@@ -14,10 +31,15 @@ fn part2(data: &Data) -> usize {
 
 #[test]
 fn test() {
-    let tests = r#""#;
+    let tests = r#"2-4,6-8
+2-3,4-5
+5-7,7-9
+2-8,3-7
+6-6,4-6
+2-6,4-8"#;
     let data = parse_input(&tests);
 
-    assert_eq!(part1(&data), 0);
+    assert_eq!(part1(&data), 2);
     assert_eq!(part2(&data), 0);
 }
 
