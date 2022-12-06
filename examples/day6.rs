3aab3ceedd8a01a985e725f1bf46a5e25efc38fc
Chris Emerson <github@mail.nosreme.org>
2022-12-06 05:40:37 +0000

    Day 6 part 1.

--- examples/day6.rs
+++ examples/day6.rs
@@ -1,12 +1,25 @@
 #[allow(unused)]
 use adventofcode2022::{get_input,parse_lines};
 
-type Data = ();
+type Data = String;
 fn parse_input(input: &str) -> Data {
+    input.into()
 }
 
 fn part1(data: &Data) -> usize {
-    unimplemented!()
+    let bytes = data.as_bytes();
+    let mut l = 0;
+    for chunk in bytes.windows(4) {
+        let mut bits = 0usize;
+        for &b in chunk {
+            bits |= 1<<((b - b'a') as usize)
+        }
+        if bits.count_ones() == 4 {
+            return l+4;
+        }
+        l += 1;
+    }
+    panic!()
 }
 fn part2(data: &Data) -> usize {
     unimplemented!()
@@ -14,11 +27,19 @@ fn part2(data: &Data) -> usize {
 
 #[test]
 fn test() {
-    let tests = r#""#;
-    let data = parse_input(&tests);
+    let tests = &[
+        ("mjqjpqmgbljsphdztnvjfqwrcgsmlb", 7, 0),
+        ("bvwbjplbgvbhsrlpgdmjqwftvncz", 5, 0),
+        ("nppdvjthqldpwncqszvftbrmjlhg", 6, 0),
+        ("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg", 10, 0),
+        ("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw", 11, 0),
+    ];
 
-    assert_eq!(part1(&data), 0);
-    assert_eq!(part2(&data), 0);
+    for &(s, res, _) in tests {
+        let data = parse_input(s);
+        assert_eq!(part1(&data), res);
+    }
+    //assert_eq!(part2(&data), 0);
 }
 
 fn main() -> std::io::Result<()>{
