1f55d7b8436abadf6490ce0e1b06221b73b54e4c
Chris Emerson <github@mail.nosreme.org>
2022-12-09 05:24:30 +0000

    Day 9 part 1.

--- examples/day9.rs
+++ examples/day9.rs
@@ -1,12 +1,66 @@
+use std::collections::HashSet;
+
 #[allow(unused)]
 use adventofcode2022::{get_input,parse_lines,regex_parser};
 
-type Data = ();
+#[derive(Copy,Clone,PartialEq,Eq,Debug)]
+pub enum Move {
+    Up(isize),
+    Down(isize),
+    Left(isize),
+    Right(isize),
+}
+
+regex_parser!(parse_move: Move {
+    U = r#"^U (\d+)"# => |n: isize| { Move::Up(n) },
+    D = r#"^D (\d+)"# => |n: isize| { Move::Down(n) },
+    L = r#"^L (\d+)"# => |n: isize| { Move::Left(n) },
+    R = r#"^R (\d+)"# => |n: isize| { Move::Right(n) }
+});
+
+type Data = Vec<Move>;
 fn parse_input(input: &str) -> Data {
+    parse_lines(input)
 }
 
 fn part1(data: &Data) -> usize {
-    unimplemented!()
+    let mut tail_seen = HashSet::new();
+    tail_seen.insert((0, 0));
+
+    let mut hx = 0isize;
+    let mut hy = 0isize;
+    let mut tx = 0isize;
+    let mut ty = 0isize;
+
+    for mv in data {
+        let (dx, dy, &n) = match dbg!(mv) {
+            Move::Up(n) => (0, -1, n),
+            Move::Down(n) => (0, 1, n),
+            Move::Left(n) => (-1, 0, n),
+            Move::Right(n) => (1, 0, n),
+        };
+        for _ in 0..n {
+            hx += dx;
+            hy += dy;
+            let same_x = hx == tx;
+            let same_y = hy == ty;
+
+            if (tx - hx).abs() > 1 || (ty - hy).abs() > 1 {
+                // Need to do a move
+                if same_y {
+                    tx += if hx > tx { 1 } else { -1 };
+                } else if same_x {
+                    ty += if hy > ty { 1 } else { -1 };
+                } else {
+                    tx += if hx > tx { 1 } else { -1 };
+                    ty += if hy > ty { 1 } else { -1 };
+                }
+            }
+            dbg!((hx, hy, tx, ty));
+            tail_seen.insert((tx, ty));
+        }
+    }
+    tail_seen.len()
 }
 fn part2(data: &Data) -> usize {
     unimplemented!()
@@ -14,11 +68,18 @@ fn part2(data: &Data) -> usize {
 
 #[test]
 fn test() {
-    let tests = r#""#;
+    let tests = r#"R 4
+U 4
+L 3
+D 1
+R 4
+D 1
+L 5
+R 2"#;
     let data = parse_input(&tests);
 
-    assert_eq!(part1(&data), 0);
-    assert_eq!(part2(&data), 0);
+    assert_eq!(part1(&data), 13);
+    //assert_eq!(part2(&data), 0);
 }
 
 fn main() -> std::io::Result<()>{
