105f0c3c7c4bf525c769342aaa1a0ef0c2815426
Chris Emerson <github@mail.nosreme.org>
2022-12-09 05:34:07 +0000

    Day 9 part 2.

--- examples/day9.rs
+++ examples/day9.rs
@@ -33,7 +33,7 @@ fn part1(data: &Data) -> usize {
     let mut ty = 0isize;
 
     for mv in data {
-        let (dx, dy, &n) = match dbg!(mv) {
+        let (dx, dy, &n) = match mv {
             Move::Up(n) => (0, -1, n),
             Move::Down(n) => (0, 1, n),
             Move::Left(n) => (-1, 0, n),
@@ -56,14 +56,50 @@ fn part1(data: &Data) -> usize {
                     ty += if hy > ty { 1 } else { -1 };
                 }
             }
-            dbg!((hx, hy, tx, ty));
             tail_seen.insert((tx, ty));
         }
     }
     tail_seen.len()
 }
 fn part2(data: &Data) -> usize {
-    unimplemented!()
+    let mut tail_seen = HashSet::new();
+    tail_seen.insert((0, 0));
+
+    let mut rope = vec![(0isize, 0isize);10];
+
+    for mv in data {
+        let (dx, dy, &n) = match mv {
+            Move::Up(n) => (0, -1, n),
+            Move::Down(n) => (0, 1, n),
+            Move::Left(n) => (-1, 0, n),
+            Move::Right(n) => (1, 0, n),
+        };
+        for _ in 0..n {
+            rope[0].0 += dx;
+            rope[0].1 += dy;
+            for ridx in 1..10 {
+                let (hx, hy) = rope[ridx-1];
+                let (mut tx, mut ty) = rope[ridx];
+                let same_x = hx == tx;
+                let same_y = hy == ty;
+
+                if (tx - hx).abs() > 1 || (ty - hy).abs() > 1 {
+                    // Need to do a move
+                    if same_y {
+                        tx += if hx > tx { 1 } else { -1 };
+                    } else if same_x {
+                        ty += if hy > ty { 1 } else { -1 };
+                    } else {
+                        tx += if hx > tx { 1 } else { -1 };
+                        ty += if hy > ty { 1 } else { -1 };
+                    }
+                }
+                rope[ridx] = (tx, ty);
+            }
+            tail_seen.insert(rope[9]);
+        }
+    }
+    tail_seen.len()
 }
 
 #[test]
@@ -79,7 +115,18 @@ R 2"#;
     let data = parse_input(&tests);
 
     assert_eq!(part1(&data), 13);
-    //assert_eq!(part2(&data), 0);
+    assert_eq!(part2(&data), 1);
+
+    let test2 = r#"R 5
+U 8
+L 8
+D 3
+R 17
+D 10
+L 25
+U 20"#;
+    let data2 = parse_input(&test2);
+    assert_eq!(part2(&data2), 36);
 }
 
 fn main() -> std::io::Result<()>{
