2184d46f50ab774d3bd7307187efc58bbd61fdb7
Chris Emerson <github@mail.nosreme.org>
2022-12-18 08:03:38 +0000

    Day 18 part 1.

--- examples/day18.rs
+++ examples/day18.rs
@@ -1,13 +1,49 @@
+use std::collections::HashSet;
+
 #[allow(unused)]
 use adventofcode2022::{get_input,parse_lines,regex_parser,timeit};
 
-type Data = ();
+#[derive(Debug,Copy,Clone,Hash,Eq,PartialEq)]
+pub struct Droplet(isize, isize, isize);
+
+regex_parser!(parse_droplet: Droplet {
+    U = r#"^(-?\d+),(-?\d+),(-?\d+)$"# =>
+        |x: isize, y: isize, z: isize| Droplet(x, y, z)
+});
+
+type Data = Vec<Droplet>;
 fn parse_input(input: &str) -> Data {
+    parse_lines(input)
 }
 
 timeit!{
 fn part1(data: &Data) -> usize {
-    unimplemented!()
+    let s: HashSet<Droplet> = data.iter()
+                                  .cloned()
+                                  .collect();
+
+    let mut count = 0;
+    for Droplet(x, y, z) in &s {
+        if !s.contains(&Droplet(*x-1, *y, *z)) {
+            count += 1;
+        }
+        if !s.contains(&Droplet(*x, *y-1, *z)) {
+            count += 1;
+        }
+        if !s.contains(&Droplet(*x, *y, *z-1)) {
+            count += 1;
+        }
+        if !s.contains(&Droplet(*x+1, *y, *z)) {
+            count += 1;
+        }
+        if !s.contains(&Droplet(*x, *y+1, *z)) {
+            count += 1;
+        }
+        if !s.contains(&Droplet(*x, *y, *z+1)) {
+            count += 1;
+        }
+    }
+    count
 }}
 timeit!{
 fn part2(data: &Data) -> usize {
@@ -16,11 +52,23 @@ fn part2(data: &Data) -> usize {
 
 #[test]
 fn test() {
-    let tests = r#""#;
+    let tests = r#"2,2,2
+1,2,2
+3,2,2
+2,1,2
+2,3,2
+2,2,1
+2,2,3
+2,2,4
+2,2,6
+1,2,5
+3,2,5
+2,1,5
+2,3,5"#;
     let data = parse_input(&tests);
 
-    assert_eq!(part1(&data), 0);
-    assert_eq!(part2(&data), 0);
+    assert_eq!(part1(&data), 64);
+//    assert_eq!(part2(&data), 0);
 }
 
 fn main() -> std::io::Result<()>{
