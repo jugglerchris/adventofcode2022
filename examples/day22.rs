3b7aa7a58597a2573fb0aa208e01f184deeaa144
Chris Emerson <github@mail.nosreme.org>
2022-12-22 21:40:40 +0000

    WIP on more general solution for day 22 part 1.

--- examples/day22.rs
+++ examples/day22.rs
@@ -1,3 +1,5 @@
+use std::{collections::HashMap, ops::Mul};
+
 #[allow(unused)]
 use adventofcode2022::{get_input,parse_lines,regex_parser,timeit};
 
@@ -354,39 +356,230 @@ fn part1(data: &Data) -> usize {
     }
     ((y+1)*1000) + ((x+1) * 4) + (facing as usize)
 }}
-timeit!{
-fn part2(data: &Data) -> usize {
-    let mut x = data.board.rows[0].start;
-    let mut y = 0;
-    let mut facing = Facing::Right;
-    for mv in &data.moves {
-        //dbg!((x, y, facing, mv));
-        match mv {
-            Move::RotateLeft => {
-                facing = facing.turn_left();
-            }
-            Move::RotateRight => {
-                facing = facing.turn_right();
-            }
-            Move::Forward(dist) => {
-                for _ in 0..*dist {
-                    let (newfacing, newx, newy) = (data.board.forward_cube(x, y, facing));
-                    /*
-                    if (x, y) != (newx, newy) {
-                        dbg!((newx, newy, newfacing.turn_around()));
-                        let (back, oldx, oldy) = data.board.forward_cube(newx, newy, newfacing.turn_around());
-                        dbg!(newfacing.turn_around());
-                        assert_eq!((oldx, oldy, back.turn_around()), (x, y, facing));
-                    }
-                    */
-                    facing = newfacing;
-                    x = newx;
-                    y = newy;
-                }
-            }
+
+#[derive(Clone, Eq, PartialEq)]
+struct Matrix {
+    m: [i8; 9],
+}
+
+#[derive(Hash, Clone, Eq, PartialEq, Debug)]
+struct Vector {
+    v: [i8; 3],
+}
+
+impl std::fmt::Debug for Matrix {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        f.debug_struct("Matrix").field("m", &self.m).finish()
+    }
+}
+
+impl Matrix {
+    pub fn identity() -> Matrix {
+        Matrix {
+            m: [1, 0, 0,
+                0, 1, 0,
+                0, 0, 1],
         }
     }
-    ((y+1)*1000) + ((x+1) * 4) + (facing as usize)
+
+    pub fn fold_right(&self) -> Matrix {
+        self * &Matrix {
+            m: [0, 0, 1,
+                0, 1, 0,
+                -1, 0, 0,
+            ]
+        }
+    }
+
+    pub fn fold_left(&self) -> Matrix {
+        self * &Matrix {
+            m: [0, 0, -1,
+                0, 1, 0,
+                1, 0, 0,
+            ]
+        }
+    }
+
+    pub fn fold_down(&self) -> Matrix {
+        &Matrix {
+            m: [1, 0, 0,
+                0, 0, 1,
+                0, -1, 0,
+            ]
+        } * self
+    }
+}
+
+impl Vector {
+    pub fn new(x: i8, y: i8, z: i8) -> Vector {
+        Vector {
+            v: [x, y, z],
+        }
+    }
+}
+
+impl Mul<&Matrix> for &Matrix {
+    type Output = Matrix;
+
+    fn mul(self, rhs: &Matrix) -> Self::Output {
+        let mut m: [i8;9] = Default::default();
+        for y in 0..3 {
+            for x in 0..3 {
+                let mut acc = 0;
+                for i in 0..3 {
+                    acc += rhs.m[i*3 + x] * self.m[y*3 + i];
+                }
+                m[y*3+x] = acc;
+            }
+        }
+        Matrix { m }
+    }
+}
+
+impl Mul<Vector> for &Matrix {
+    type Output = Vector;
+
+    fn mul(self, rhs: Vector) -> Self::Output {
+        let mut v: [i8;3] = Default::default();
+        for y in 0..3 {
+            let mut acc = 0;
+            for i in 0..3 {
+                acc += rhs.v[i] * self.m[y*3 + i];
+            }
+            v[y] = acc;
+        }
+        Vector { v }
+    }
+}
+
+#[test]
+fn test_vector() {
+    let iden = Matrix::identity();
+    assert_eq!(&iden * Vector::new(-1, -1, 1), Vector::new(-1, -1, 1));
+    assert_eq!(&iden * Vector::new(1, 1, 1), Vector::new(1, 1, 1));
+    assert_eq!(&iden * Vector::new(-1, 1, 1), Vector::new(-1, 1, 1));
+    assert_eq!(&iden * Vector::new(1, -1, 1), Vector::new(1, -1, 1));
+
+    let fr = iden.fold_right();
+    assert_eq!(&fr * Vector::new(-1, -1, 1), Vector::new(1, -1, 1));
+    assert_eq!(&fr * Vector::new(1, 1, 1), Vector::new(1, 1, -1));
+    assert_eq!(&fr * Vector::new(-1, 1, 1), Vector::new(1, 1, 1));
+    assert_eq!(&fr * Vector::new(1, -1, 1), Vector::new(1, -1, -1));
+
+    let fl = iden.fold_left();
+    assert_eq!(&fl * Vector::new(-1, -1, 1), Vector::new(-1, -1, -1));
+    assert_eq!(&fl * Vector::new(1, 1, 1), Vector::new(-1, 1, 1));
+    assert_eq!(&fl * Vector::new(-1, 1, 1), Vector::new(-1, 1, -1));
+    assert_eq!(&fl * Vector::new(1, -1, 1), Vector::new(-1, -1, 1));
+
+    let fd = iden.fold_down();
+    assert_eq!(&fd * Vector::new(-1, -1, 1), Vector::new(-1, 1, 1));
+    assert_eq!(&fd * Vector::new(1, 1, 1), Vector::new(1, 1, -1));
+    assert_eq!(&fd * Vector::new(-1, 1, 1), Vector::new(-1, 1, -1));
+    assert_eq!(&fd * Vector::new(1, -1, 1), Vector::new(1, 1, 1));
+
+    assert_eq!(&(&(&fd * &fd) * &fd) * &fd, iden);
+    assert_eq!(&(&(&fl * &fl) * &fl) * &fl, iden);
+    assert_eq!(&(&(&fr * &fr) * &fr) * &fr, iden);
+}
+
+struct CubeMap<'d> {
+    data: &'d Data,
+    side_len: usize,
+    square_mappings: HashMap<(usize, usize), Matrix>,
+    coord_to_corner_name: HashMap<Vector, u8>,
+}
+
+impl<'d> CubeMap<'d> {
+    pub fn new(data: &'d Data) -> CubeMap<'d> {
+        let side_len =
+            data.board
+                .rows
+                .iter()
+                .map(|r| r.spaces.len())
+                .min()
+                .unwrap();
+
+        let x = data.board.rows[0].start;
+        let y = 0;
+        let matrix = Matrix::identity();
+
+        let mut work = vec![(x, y, matrix)];
+        let mut square_mappings = HashMap::new();
+
+        let coord_to_corner_name: HashMap<Vector, u8> =
+            [(Vector::new(-1, -1, 1), b'A'),
+             (Vector::new(1, -1, 1), b'B'),
+             (Vector::new(-1, 1, 1), b'C'),
+             (Vector::new(1, 1, 1), b'D'),
+             (Vector::new(-1, -1, -1), b'E'),
+             (Vector::new(1, -1, -1), b'F'),
+             (Vector::new(-1, 1, -1), b'G'),
+             (Vector::new(1, 1, -1), b'H'),
+            ].into_iter()
+             .collect();
+
+        while let Some((x, y, matrix)) = work.pop() {
+            dbg!((x, y, &matrix));
+            if square_mappings.contains_key(&(x, y)) {
+                assert_eq!(square_mappings.get(&(x, y)).unwrap(), &matrix);
+                continue;
+            }
+            square_mappings.insert((x, y), matrix.clone());
+            if data.board.has_space(x+side_len, y) {
+                work.push((x+side_len, y, matrix.fold_right()));
+            }
+            if data.board.has_space(x, y+side_len) {
+                work.push((x, y+side_len, matrix.fold_down()));
+            }
+            if x > 0 && data.board.has_space(x-side_len, y) {
+                work.push((x-side_len, y, matrix.fold_left()));
+            }
+        }
+
+        CubeMap {
+            data,
+            side_len,
+            square_mappings,
+            coord_to_corner_name,
+        }
+    }
+
+    pub fn print_plain(&self) {
+        for (y, row) in self.data.board.rows.iter().enumerate() {
+            for _ in 0..row.start {
+                print!(" ");
+            }
+            for (ox, sp) in row.spaces.iter().enumerate() {
+                let x = ox + row.start;
+                let local_x = x % self.side_len;
+                let local_y = y % self.side_len;
+                if (local_x == 0 || local_x == self.side_len-1) &&
+                   (local_y == 0 || local_y == self.side_len-1) {
+                    let sq_x = x-(x%self.side_len);
+                    let sq_y = y-(y%self.side_len);
+                    let mat = self.square_mappings.get(&(sq_x, sq_y)).unwrap();
+                    let x3 = if local_x == 0 { -1 } else { 1 };
+                    let y3 = if local_y == 0 { -1 } else { 1 };
+                    let coord = mat * Vector::new(x3, y3, 1);
+                    print!("{}", *self.coord_to_corner_name.get(&coord).unwrap() as char);
+                } else {
+                    match sp {
+                        Space::Wall => print!("#"),
+                        Space::Empty => print!("."),
+                    }
+                }
+            }
+            println!("");
+        }
+    }
+}
+
+timeit!{
+fn part2(data: &Data) -> usize {
+    let cube_map = CubeMap::new(data);
+    cube_map.print_plain();
+    0
 }}
 
 #[test]
@@ -408,7 +601,7 @@ fn test() {
     let data = parse_input(&tests);
 
     assert_eq!(part1(&data), 6032);
-//    assert_eq!(part2(&data), 0);
+    assert_eq!(part2(&data), 5031);
 }
 
 fn main() -> std::io::Result<()>{
